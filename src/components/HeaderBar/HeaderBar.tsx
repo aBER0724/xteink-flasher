f0edefe0d8182d31bf8d9520571b5406aab6f9f8
Dave Allie <dave@daveallie.com>
2025-11-21 00:36:04 +1100

    Fix header bar not showing correct theme

--- src/components/HeaderBar/HeaderBar.tsx
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -1,21 +1,29 @@
 'use client';
 
 import React from 'react';
-import { Box, Container, Flex, Heading, IconButton } from '@chakra-ui/react';
-import { ColorModeButton, useColorModeValue } from '@/components/ui/color-mode';
-import { LuGithub } from 'react-icons/lu';
+import {
+  Box,
+  ClientOnly,
+  Container,
+  Flex,
+  Heading,
+  IconButton,
+} from '@chakra-ui/react';
+import { ColorModeButton } from '@/components/ui/color-mode';
+import { LuGithub, LuSun } from 'react-icons/lu';
 
 export default function HeaderBar() {
   return (
-    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+    <Box bg="header-bar.bg" px={4}>
       <Container maxW="3xl">
         <Flex h={16} alignItems="center" justifyContent="space-between">
-          <Heading size="md" color={useColorModeValue('gray.700', 'gray.300')}>
+          <Heading size="md" color="header-bar.fg">
             Xteink Flash Tools
           </Heading>
 
           <Flex alignItems="center" gap={2}>
             <IconButton
+              size="sm"
               variant="outline"
               onClick={() =>
                 window.open(
@@ -23,11 +31,34 @@ export default function HeaderBar() {
                   '_blank',
                 )
               }
+              css={{
+                _icon: {
+                  width: '5',
+                  height: '5',
+                },
+              }}
               aria-label="Go to Github repo"
             >
               <LuGithub />
             </IconButton>
-            <ColorModeButton variant="outline" />
+            <ClientOnly
+              fallback={
+                <IconButton
+                  size="sm"
+                  variant="outline"
+                  css={{
+                    _icon: {
+                      width: '5',
+                      height: '5',
+                    },
+                  }}
+                >
+                  <LuSun />
+                </IconButton>
+              }
+            >
+              <ColorModeButton variant="outline" />
+            </ClientOnly>
           </Flex>
         </Flex>
       </Container>
